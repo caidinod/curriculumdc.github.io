c6b394b6f7cea6ca40a7169afec447a4f0daba11
caidinod <123591274+caidinod@users.noreply.github.com>
2023-01-29 17:40:06 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+document.getElementById("mostrar").onclick = function mostrar() { document.getElementById("datos").innerHTML = "40 años. Argentino. DNI 29091563. Fecha de nacimiento: 17.02.1982. Domicilio: Suipacha 235 Alejandro Korn, Partido de San Vicente, Buenos Aires, Argentina. Tel. (011) 15-5516-7451. E-mail: diego.caidino@yahoo.com.ar";}
+document.getElementById("ocultar").onclick = function ocultar() {document.getElementById("datos").innerHTML = "";}
+
+document.getElementById("mostrar4").onclick = function mostrar() {document.getElementById("aptitudes").innerHTML = "* Alto nivel de responsabilidad y eficiencia.<br>* Compromiso para el logro de objetivos establecidos.<br>* Gran capacidad de relaciones interpersonales y administración de personal.<br>* Capacidad de toma de decisiones.<br>* Excelente desempeño y autocontrol personal en situaciones adversas.<br>* Gran capacidad de actuar operativamente.";}
+document.getElementById("ocultar4").onclick = function ocultar() {document.getElementById("aptitudes").innerHTML = "";}
+
+
+    
